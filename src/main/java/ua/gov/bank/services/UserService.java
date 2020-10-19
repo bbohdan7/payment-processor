e2bdc82733b7ca5c346b0f371a736adc66f74ceb
Bogdan <zbohdan7@gmail.com>
2020-10-19 12:45:52 +0300

    few changes

--- src/main/java/ua/gov/bank/services/UserService.java
+++ src/main/java/ua/gov/bank/services/UserService.java
@@ -5,10 +5,7 @@ import ua.gov.bank.model.User;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
@@ -33,6 +30,12 @@ public class UserService extends Service<User> {
     @GET
     public List<User> all(){
         return super.all();
+    }
+
+    @GET
+    @Path("{id}")
+    public User find(@PathParam("id") Integer id){
+        return super.find(id);
     }
 
 }
